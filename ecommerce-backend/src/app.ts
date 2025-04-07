be33110f887847a4d735b65ef335b8cb07e5cb7f
lagwalsahil21821 <lagwalsahil7978@gmail.com>
2025-04-08 01:12:55 +0530

    Add user routes

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,18 +1,29 @@
 import express from 'express'
+import { connectDB } from './utils/features.js';
 
 // importing routes
 import userRoute from './routes/user.js'
+import { errorMiddleware } from './middlewares/error.js';
 
 const port = 3000;
+
+// connect to database
+connectDB();
+
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
+    res.send("API working with /api/v1")
     res.end("Hello World");
-})
+});
 
 // using Routes
-app.use("/api/v1/user", userRoute)
+app.use("/api/v1/user", userRoute);
+
+// middleware for error handling
+app.use(errorMiddleware);
 
 app.listen(port, () => {
     console.log(`Node is working on http://localhost:${port}`)
-})
+});
--- ecommerce-backend/src/controllers/user.ts
+++ ecommerce-backend/src/controllers/user.ts
@@ -1,19 +1,77 @@
 import { Request, Response, NextFunction } from "express";
+import { TryCatch } from "../middlewares/error.js";
 import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
+import ErrorHandler from "../utils/utility-class.js";
 
-export const newUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {} = req.body;
+export const newUser = TryCatch(
+  async (
+    req: Request<{}, {}, NewUserRequestBody>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { name, email, photo, gender, _id, dob } = req.body;
 
-        const user = await User.create({
+    let user = await User.findById(_id);
 
-        })
-
-        return res.status(200).json({
-            success: true,
-            message: `Welcome, ${user.name}`,
-        })
-    } catch (error) {
-        console.log(error);
+    // if user already exists
+    if(user) {
+      return res.status(200).json({
+        success: true,
+        message: `Welcome back, ${user.name}`,
+      })
     }
-}
+
+    if(!name || !_id || !photo || !gender || !email || !dob)
+      return next(new ErrorHandler("Please fill all the fields", 400));
+
+    user = await User.create({
+      name,
+      email,
+      photo,
+      gender,
+      _id,
+      dob: new Date(dob),
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: `Welcome, ${user.name}`,
+    });
+  }
+);
+
+export const getAllUsers = TryCatch(async (req, res, next) => {
+  const users = await User.find({});
+
+  return res.status(200).json({
+    success: true,
+    users,
+  })
+});
+
+export const getUser = TryCatch(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if(!user) {
+    return next(new ErrorHandler("Invalid User", 404));
+  }
+
+  return res.status(200).json({
+    success: true,
+    user
+  })
+});
+
+export const deleteUser = TryCatch(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if(!user) {
+    return next(new ErrorHandler("Invalid User", 404));
+  }
+
+  user.deleteOne();
+
+  return res.status(200).json({
+    success: true,
+    message: "User deleted successfully!"
+  })
+});
