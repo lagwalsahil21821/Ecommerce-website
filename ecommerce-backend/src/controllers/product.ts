24104e38790ba8617a92e4611303cf97a19c2a18
lagwalsahil21821 <lagwalsahil7978@gmail.com>
2025-04-10 16:28:49 +0530

    Add 'get all product with filters' API

--- ecommerce-backend/src/controllers/product.ts
+++ ecommerce-backend/src/controllers/product.ts
@@ -1,10 +1,10 @@
 import { Request } from "express";
+import { FilterQuery } from "mongoose";
 import { TryCatch } from "../middlewares/error.js";
-import { NewProductRequestBody } from "../types/types.js";
+import { BaseQuery, NewProductRequestBody, SearchRequestQuery } from "../types/types.js";
 import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
-
 
 export const newProduct = TryCatch(async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
     const { name, category, price, stock } = req.body;
@@ -113,4 +113,47 @@ export const deleteProduct = TryCatch(async(req, res, next) => {
         success: true,
         message: "Product deleted successfully",
     });
+});
+
+export const getAllProduct = TryCatch(async (req: Request<{}, {}, {}, SearchRequestQuery>, res, next) => {
+    const { price, category, sort, search } = req.query;
+    const page = Number(req.query.page);
+
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page - 1) * limit;
+
+    const baseQuery: FilterQuery<BaseQuery> = {};
+
+    if(search) {
+        baseQuery.name = {
+            $regex: search,
+            $options: 'i',
+        };
+    }
+
+    if(price) {
+        baseQuery.price = {
+            $lte: Number(price),
+        };
+    }
+
+    if(category) baseQuery.category = category;
+
+    const productsPromise = Product.find(baseQuery)
+        .sort(sort && { price: sort === "asc" ? 1 : -1 })
+        .limit(limit)
+        .skip(skip);
+
+    const [products, filteredOnlyProduct] = await Promise.all([
+        productsPromise, 
+        Product.find(baseQuery),
+    ]);
+
+    const totalPages = Math.ceil(filteredOnlyProduct.length / limit);
+
+    return res.status(200).json({
+        success: true,
+        products,
+        totalPages,
+    })
 });
--- ecommerce-backend/src/routes/product.ts
+++ ecommerce-backend/src/routes/product.ts
@@ -1,5 +1,14 @@
-import express from 'express'
-import { deleteProduct, getAdminProducts, getAllCategories, getLatestProducts, getSingleProduct, newProduct, updateProduct } from '../controllers/product.js';
+import express from 'express';
+import {
+  deleteProduct,
+  getAdminProducts,
+  getAllCategories,
+  getAllProduct,
+  getLatestProducts,
+  getSingleProduct,
+  newProduct,
+  updateProduct,
+} from '../controllers/product.js';
 import { adminOnly } from '../middlewares/auth.js';
 import { singleUpload } from '../middlewares/multer.js';
 
@@ -17,12 +26,14 @@ app.get("/categories", getAllCategories);
 // route get all the products - /api/v1/product/admin-products
 app.get("/admin-products", adminOnly, getAdminProducts);
 
+// route to get all the products with filters - /api/v1/product/all
+app.get("/all", getAllProduct);
+
 // route - /api/v1/product/:id
 app
-    .route("/:id")
-    .get(getSingleProduct)
-    .put(adminOnly, singleUpload, updateProduct)
-    .delete(adminOnly, deleteProduct);
-
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(adminOnly, singleUpload, updateProduct)
+  .delete(adminOnly, deleteProduct);
 
 export default app;
